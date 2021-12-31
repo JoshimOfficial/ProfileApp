7cc83ed5eababc059525c7a4d0229c2b9e1665a1
Joshim Uddin <70097642+JoshimOfficial@users.noreply.github.com>
2022-01-01 02:41:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -356,21 +356,21 @@ experience.addEventListener("click", ()=> {
 
 
 
-// function getFullscreenElement() {
-//     return document.fullscreenElement   //standard property
-//     || document.webkitFullscreenElement //safari/opera support
-//     || document.mozFullscreenElement    //firefox support
-//     || document.msFullscreenElement;    //ie/edge support
-//  }
+function getFullscreenElement() {
+    return document.fullscreenElement   //standard property
+    || document.webkitFullscreenElement //safari/opera support
+    || document.mozFullscreenElement    //firefox support
+    || document.msFullscreenElement;    //ie/edge support
+ }
 
-//  function toggleFullscreen() {
-//     if(getFullscreenElement()) {
-//        document.exitFullscreen();
-//     }else {
-//   document.documentElement.requestFullscreen().catch(console.log);
-//     }
-//  }
-//  document.addEventListener('dblclick', () => {
-//     toggleFullscreen();
-//  });
+ function toggleFullscreen() {
+    if(getFullscreenElement()) {
+       document.exitFullscreen();
+    }else {
+  document.documentElement.requestFullscreen().catch(console.log);
+    }
+ }
+ document.addEventListener('dblclick', () => {
+    toggleFullscreen();
+ });
 
